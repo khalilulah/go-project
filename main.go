f94b2144583b3561b563e7103e6d7ce3c991567f
khalilulah <alausakhalil@gmail.com>
2025-09-25 16:44:08 +0100

    fibre added

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main(){
 	fmt.Println("hello world")
+	fmt.Println(intDivision(10,2))
+}
+
+// func printme(printValue string){
+// 	fmt.Println(printValue)
+// }
+
+func intDivision(numerator int, denominator int)(int, int){
+	var result int = numerator/denominator
+	var remainder int = numerator%denominator
+	return result, remainder
 }
