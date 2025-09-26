42272260bba7334293fef4c880df1a2655f6ef1f
khalilulah <alausakhalil@gmail.com>
2025-09-26 17:39:50 +0100

    added the patch endpoint

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
-	// app.Patch("/api/todos/:id", updateTodo)
+	app.Patch("/api/todos/:id", updateTodo)
 	// app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
@@ -117,5 +117,21 @@ func createTodo(c *fiber.Ctx) error{
 
 	return  c.Status(201).JSON(todo)
 }
-// func updateTodo(c *fiber.Ctx) error{}
+func updateTodo(c *fiber.Ctx) error{
+
+	id := c.Params("id")
+	ObjectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil{
+		return c.Status(400).JSON(fiber.Map{"error": "invalid id"})
+	}
+	filter := bson.M{"_id": ObjectID}
+	update := bson.M{"$set":bson.M{"completed":true}}
+	_,err = collection.UpdateOne(context.Background(),filter,update)
+
+	if err != nil {
+		return  err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
 // func deleteTodo(c *fiber.Ctx) error{}
